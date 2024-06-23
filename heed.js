ab96af0b839191392b1ffe7e7a2dfe726dcd7b82
Thomas Heslin <13917706+tjheslin1@users.noreply.github.com>
2024-06-23 22:41:32 +0100

    working printing weapon name

--- heed.js
+++ heed.js
@@ -2,7 +2,35 @@ on('ready', function() {
     
     on('change:attribute', (obj) => {
         if (obj.get('name') === 'attack_1_clicked') {
-            sendChat("Tom", "clickeded");
+            let characterId = obj.get('_characterid');
+            // let weapon = obj.get('attack_1_desc');
+            
+            let attr = findObjs({
+                name: 'attack_1_desc',
+                _type: 'attribute',
+                _characterid: characterId
+            })[0];
+            
+            console.log(attr);
+            
+            let att = null;
+            if (attr) {
+                att = attr.get("current");
+            }
+
+            sendChat(characterId, "clickeded " + obj.attack_1_desc + " " + att);
         };
     });
 });
+
+const weapons = {
+    "shortsword": {
+        1: "No damage",
+        2: "1",
+        3: "2",
+        4: "3",
+        5: "4",
+        6: "5",
+        7: "6",
+    }
+};
